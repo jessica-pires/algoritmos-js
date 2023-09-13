a793aad7ea0d8919d6962b5cd7033a43c79077f4
jessica-pires <jessica_dutra19@outlook.com>
2023-09-13 17:10:19 -0300

    feito

---
+++ ordenacao.js/listaLivros.js
@@ -0,0 +1,26 @@
+ const livros = [
+ {
+    titulo: "Javascript", 
+    preco: 25,
+},
+{
+    titulo: "PHP",
+    preco: 15,
+},
+{
+    titulo:"java",
+    preco: 30,
+},
+{
+    titulo: "Elixir",
+    preco: 50
+},
+{
+    titulo:"Go",
+    preco: 45
+},
+{
+    titulo:"Python",
+    preco:20
+}
+]
